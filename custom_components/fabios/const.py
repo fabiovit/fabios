51cfd9fb38b05d02ce26e2bf2f6a7aea646cac01
Fabio Vittori <63154975+fabiovit@users.noreply.github.com>
2026-08-31 14:41:41 +0200

    release: bump frontend cache keys to 2.1.1

--- custom_components/fabios/const.py
+++ custom_components/fabios/const.py
@@ -1,13 +1,13 @@
 DOMAIN = "fabios"
-VERSION = "2.1.0"
+VERSION = "2.1.1"
 STORAGE_KEY = "fabios.data"
 STORAGE_VERSION = 1
 PANEL_URL = "fabios"
 STATIC_URL = "/fabios_static"
-PANEL_MODULE_URL = f"{STATIC_URL}/fabios-panel.js?v=2.1.0"
+PANEL_MODULE_URL = f"{STATIC_URL}/fabios-panel.js?v=2.1.1"
 DEFAULT_CURRENCY = "EUR"
 DEFAULT_GROUP_NAME = "Casa"
 DEFAULT_CATEGORY = "Altro"
 
 LITE_PANEL_URL = "fabios-lite"
-LITE_PANEL_MODULE_URL = f"{STATIC_URL}/fabios-lite.js?v=2.1.0"
+LITE_PANEL_MODULE_URL = f"{STATIC_URL}/fabios-lite.js?v=2.1.1"
