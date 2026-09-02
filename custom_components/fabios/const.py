542ce76670001359f8c08ed52f6dc0db6e057ea9
Fabio Vittori <63154975+fabiovit@users.noreply.github.com>
2026-09-02 16:53:22 +0200

    chore: update frontend cache keys for 2.1.5

--- custom_components/fabios/const.py
+++ custom_components/fabios/const.py
@@ -1,13 +1,13 @@
 DOMAIN = "fabios"
-VERSION = "2.1.4"
+VERSION = "2.1.5"
 STORAGE_KEY = "fabios.data"
 STORAGE_VERSION = 1
 PANEL_URL = "fabios"
 STATIC_URL = "/fabios_static"
-PANEL_MODULE_URL = f"{STATIC_URL}/fabios-panel.js?v=2.1.4"
+PANEL_MODULE_URL = f"{STATIC_URL}/fabios-panel.js?v=2.1.5"
 DEFAULT_CURRENCY = "EUR"
 DEFAULT_GROUP_NAME = "Casa"
 DEFAULT_CATEGORY = "Altro"
 
 LITE_PANEL_URL = "fabios-lite"
-LITE_PANEL_MODULE_URL = f"{STATIC_URL}/fabios-lite.js?v=2.1.4"
+LITE_PANEL_MODULE_URL = f"{STATIC_URL}/fabios-lite.js?v=2.1.5"
